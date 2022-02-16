421e90c00bae772b23af3a58afe53ce219bc6894
p112627701 <85160237+p112627701@users.noreply.github.com>
2022-02-16 08:28:35 +0800

    Create admin.php
    新增繁體中文

---
+++ resources/lang/zh_TW/admin.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'Admin' => '後台',
+    'Projects management' => '項目管理',
+    'Users management' => '用戶管理',
+    'Groups management' => '團隊管理',
+    'Plugins management' => '插件管理',
+    'Manage projects' => '項目管理',
+    'Settings' => '設置',
+    'Application settings' => '系統設置',
+    'Theme settings' => '主題设置',
+    'Localization settings' => '國際化設置',
+];
